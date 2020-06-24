52e61e4008fdf800f9ef7d0ef67c173445d2a05c
BOURA Troulé <BOURA Troul@BOURA-Troul>
2020-06-24 16:36:27 +0200

    S

--- src/InversionContenu/InversionContenu.java
+++ src/InversionContenu/InversionContenu.java
@@ -4,8 +4,13 @@ public class InversionContenu {
 
 	public static void main(String[] args) {
 		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
-	• Créer un tableau arrayCopy et copier tous les éléments de array dans arrayCopy mais dans l’ordre inverse.
-	• Afficher l’ensemble des éléments des 2 tableaux
+	/****Créer un tableau arrayCopy et copier tous les éléments de array dans arrayCopy mais dans l’ordre inverse.**/
+		int[] arrayCopy = new int[array.length];
+		for(int i = array.length -1; i >= 0; i--) {
+			 arrayCopy[i] = array[i];
+			System.out.println(array[i]);
+			System.out.println(arrayCopy[i]);
+		}
 
 	}
 
