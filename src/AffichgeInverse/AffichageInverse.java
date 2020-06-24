0d26d29ab99fbb2e9747c12262434477765a1e02
BOURA Troulé <BOURA Troul@BOURA-Troul>
2020-06-24 15:57:10 +0200

    suite

---
+++ src/AffichgeInverse/AffichageInverse.java
@@ -0,0 +1,28 @@
+package AffichgeInverse;
+
+public class AffichageInverse {
+
+	public static void main(String[] args) {
+	/*******Afficher l’ensemble des éléments du tableau grâce à une boucle***/
+		 int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4}; 
+		 for(int i = 0; i <= array.length - 1; i++)	{
+			 System.out.println(array[i]);
+		 }	 
+		 	System.out.println("--------ExoSuivant--------");
+		 
+	/*******Afficher l’ensemble des éléments dans l’ordre inverse du tableau**/
+		 for(int i = array.length -1; i >= 0; i--) {
+			 System.out.println(array[i]);
+		 }
+		 
+		 System.out.println("----------ExoSuivant------");
+	/***Créer un tableau arrayCopy et copier tous les éléments de array dans arrayCopy***/
+		 int[] arrayCopy = new int[array.length];
+		 for(int i = 0; i <= array.length; i++) {
+			 arrayCopy[i] = array[i];
+			 System.out.println(arrayCopy[i]);
+		 }
+		 		 
+		}
+
+	}
