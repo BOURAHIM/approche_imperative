740bf74e194ba6277b94f07519494cbe834bc8c9
BOURA Troulé <BOURA Troul@192.168.1.26>
2020-06-25 10:22:29 +0200

    m

---
+++ src/RechercheMax/RechercheMax.java
@@ -0,0 +1,15 @@
+package RechercheMax;
+
+public class RechercheMax {
+
+	public static void main(String[] args) {
+		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+	
+		/*****Rechercher le plus grand élément du tableau****/
+		for(int i = 0; i <= array.length; i++) {
+			if()
+		}
+
+	}
+
+}
